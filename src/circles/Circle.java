5028127e68131234c45e7f4f1bb3a9d9578e401e
aSwanting <gab.damico@gmail.com>
2024-04-17 23:21:27 +0200

    origin point

--- src/circles/Circle.java
+++ src/circles/Circle.java
@@ -1,22 +1,56 @@
 package circles;
 
 class Circle {
-    double d, r, x, y;
+    double d, r, x, y, vx, vy;
 
-    public Circle(double d, double x, double y) {
+    public Circle(double d, double x, double y, double vx, double vy) {
         this.d = d;
         this.r = d / 2;
-        this.x = x - r;
-        this.y = y - r;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+
+        System.out.println(toString());
     }
 
-    public void move1() {
-        this.x++;
-        this.y++;
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "d=" + d +
+                ", r=" + r +
+                ", x=" + x +
+                ", y=" + y +
+                ", vx=" + vx +
+                ", vy=" + vy +
+                '}';
     }
 
-    public void move2() {
-        this.x--;
-        this.y--;
+    public void move(int w, int h) {
+
+        if (this.x < this.r) {
+            this.x = this.r;
+            this.vx *= -1;
+        }
+
+        if (this.x > w - this.r) {
+            this.x = w - this.r;
+            this.vx *= -1;
+        }
+
+        if (this.y < this.r) {
+            this.y = this.r;
+            this.vy *= -1;
+        }
+
+        if (this.y > h - this.r) {
+            this.y = h - this.r;
+            this.vy *= -1;
+        }
+
+        this.x += this.vx;
+        this.y += this.vy;
+
     }
+
 }
--- src/circles/TestPanel.java
+++ src/circles/TestPanel.java
@@ -10,9 +10,11 @@ public class TestPanel extends JPanel {
     Circle c1, c2;
 
     public TestPanel(int w, int h) {
+        this.w = w;
+        this.h = h;
         this.setPreferredSize(new Dimension(w, h));
-        c1 = new Circle(100, 0, 0);
-        c2 = new Circle(100, w, h);
+        c1 = new Circle(60, 110, 210, 1, 1);
+//        c2 = new Circle(100, w, h, -1, -1);
     }
 
     @Override
@@ -22,20 +24,20 @@ public class TestPanel extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         drawCircle(g2d, c1, new Color(0, 0, 0, 128));
-        drawCircle(g2d, c2, new Color(0, 0, 0, 128));
+//        drawCircle(g2d, c2, new Color(0, 0, 0, 128));
 
     }
 
     private void drawCircle(Graphics2D g2d, Circle c, Color color) {
-        Shape circle = new Ellipse2D.Double(c.x, c.y, c.d, c.d);
+        Shape circle = new Ellipse2D.Double(c.x - c.r, c.y - c.r, c.d, c.d);
         g2d.setColor(color);
         g2d.fill(circle);
     }
 
     public void run() throws InterruptedException {
         while (true) {
-            c1.move1();
-            c2.move2();
+            c1.move(getWidth(), getHeight());
+//            c2.move(w, h);
             repaint();
             Thread.sleep(6);
         }
